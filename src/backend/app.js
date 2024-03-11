4c7ee09f1aa1aa916066b68bef48a9c76559e564
Pankaj Kumar Singh <pankaj28843@gmail.com>
2024-03-11 15:42:48 +0100

    refactor: update backend routing
    - Refactor backend routing by replacing app with router for better routing management

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,8 +2,6 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import mealsRouter from "./api/meals.js";
-import reservationsRouter from "./api/reservations.js";
-import reviewsRouter from "./api/reviews.js";
 import knex from "./database.js"; // Import knex using CommonJS syntax
 
 const app = express();
@@ -27,26 +25,26 @@ app.use(express.json());
 
 app.use(cors());
 
-router.use("/api/meals", mealsRouter);
-router.use("/api/reservations", reservationsRouter);
-router.use("/api/reviews", reviewsRouter);
+app.use(router);
 
-app.get("/future-meals", async (req, res) => {
+router.use("/api/meals", mealsRouter);
+
+router.get("/future-meals", async (req, res) => {
     const futureMeals = await knex("meal").where("when", ">", knex.fn.now());
     res.json(futureMeals || []);
 });
 
-app.get("/past-meals", async (req, res) => {
+router.get("/past-meals", async (req, res) => {
     const pastMeals = await knex("meal").where('when', '<', knex.raw('NOW()'));
     res.json(pastMeals || []);
 });
 
-app.get("/all-meals", async (req, res) => {
+router.get("/all-meals", async (req, res) => {
     const allMeals = await knex("meal").select("*");
     res.json(allMeals || []);
 });
 
-app.get("/first-meal", async (req, res) => {
+router.get("/first-meal", async (req, res) => {
     const firstMeal = await knex("meal").orderBy("id").first();
     if (firstMeal) {
         res.json(firstMeal);
@@ -55,7 +53,7 @@ app.get("/first-meal", async (req, res) => {
     }
 });
 
-app.get("/last-meal", async (req, res) => {
+router.get("/last-meal", async (req, res) => {
     const lastMeal = await knex("meal").orderBy("id", "desc").first();
     if (lastMeal) {
         res.json(lastMeal);
